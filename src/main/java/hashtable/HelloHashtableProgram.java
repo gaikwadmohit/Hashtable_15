124e4331730335aa7901bff9182859ea874e23ff
unknown <mohit16gaikwad@gmail.com>
2022-04-17 11:28:00 +0530

    Welcome program

---
+++ src/main/java/hashtable/HelloHashtableProgram.java
@@ -0,0 +1,10 @@
+package hashtable;
+
+public class HelloHashtableProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to hashtable program");
+	}
+
+}
